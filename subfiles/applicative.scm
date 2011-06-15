f7c3b6b9f66d358882a62f27e8dd3360b09604ba
David Banks <amoebae@gmail.com>
2011-06-15 16:33:51 +0100

    valid: applicative

--- subfiles/applicative.scm
+++ subfiles/applicative.scm
@@ -1,10 +1,6 @@
 ; This file is part of SINK, a Scheme-based Interpreter for Not-quite Kernel
 ; Copyright (c) 2009 John N. Shutt
 
-(set-version (list 0.0 0)
-             (list 0.1 0))
-(set-revision-date 2007 8 4)
-
 ;;;;;;;;;;;;;;;;
 ; applicatives ;
 ;;;;;;;;;;;;;;;;
@@ -15,43 +11,51 @@
 ; and the accessor is called "unwrap" instead of "get-applicative-underlying".
 ;
 
-(define wrap
-  (lambda (combiner)
-    (let ((appv  (let ((name  (list #t)))
-                   (lambda (message)
-                     (case message
-                       ((type)       'applicative)
-                       ((name)       name)
-                       ((underlying) combiner))))))
-      (designate-name-inheritor! appv combiner)
-      appv)))
+(library (subfiles applicative)
+  (export)
+  (import (rnrs))
 
-(define applicative? (make-object-type-predicate 'applicative))
+; XXX
+;; (define wrap
+;;   (lambda (combiner)
+;;     (let ((appv  (let ((name  (list #t)))
+;;                    (lambda (message)
+;;                      (case message
+;;                        ((type)       'applicative)
+;;                        ((name)       name)
+;;                        ((underlying) combiner))))))
+;;       (designate-name-inheritor! appv combiner)
+;;       appv)))
+
+; XXX
+;; (define applicative? (make-object-type-predicate 'applicative))
 
 (define unwrap (lambda (x) (x 'underlying)))
 
 ;
 ;
 ;
-(define unary-predicate->applicative
-  (lambda x
-    (wrap (apply unary-predicate->operative x))))
 
-(define binary-predicate->applicative
-  (lambda x
-    (wrap (apply binary-predicate->operative x))))
+; XXX: depends on WRAP
+;; (define unary-predicate->applicative
+;;   (lambda x
+;;     (wrap (apply unary-predicate->operative x))))
 
-(define metered-action->checked-applicative
-  (lambda x
-    (wrap (apply metered-action->checked-operative x))))
+;; (define binary-predicate->applicative
+;;   (lambda x
+;;     (wrap (apply binary-predicate->operative x))))
 
-(define naive->checked-applicative
-  (lambda x
-    (wrap (apply naive->checked-operative x))))
+;; (define metered-action->checked-applicative
+;;   (lambda x
+;;     (wrap (apply metered-action->checked-operative x))))
 
-(define metered-naive->checked-applicative
-  (lambda x
-    (wrap (apply metered-naive->checked-operative x))))
+;; (define naive->checked-applicative
+;;   (lambda x
+;;     (wrap (apply naive->checked-operative x))))
+
+;; (define metered-naive->checked-applicative
+;;   (lambda x
+;;     (wrap (apply metered-naive->checked-operative x))))
 
 ;
 ; Given an action, and criteria for admissible argument-lists for that action,
@@ -59,14 +63,21 @@
 ; and either invokes the given action, or throws an error.  Shorthand for
 ; composition of wrap with action->checked-operative.
 ;
-(define action->checked-applicative
-  (lambda x
-    (wrap (apply action->checked-operative x))))
+;; (define action->checked-applicative
+;;   (lambda x
+;;     (wrap (apply action->checked-operative x))))
 
 ;
 ; Given an action, constructs an applicative whose underlying operative has
 ; that action.  Shorthand for composition of wrap with action->operative.
 ;
-(define action->applicative
-  (lambda (action)
-    (wrap (action->operative action))))
+;; (define action->applicative
+;;   (lambda (action)
+;;     (wrap (action->operative action))))
+
+; XXX
+;; (set-version (list 0.0 0)
+;;              (list 0.1 0))
+;; (set-revision-date 2007 8 4)
+
+)
