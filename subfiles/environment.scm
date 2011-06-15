7cfac735ac4bed060aefaee54af474601c7d949e
David Banks <amoebae@gmail.com>
2011-06-15 17:01:58 +0100

    valid: environment

--- subfiles/environment.scm
+++ subfiles/environment.scm
@@ -1,10 +1,6 @@
 ; This file is part of SINK, a Scheme-based Interpreter for Not-quite Kernel
 ; Copyright (c) 2009 John N. Shutt
 
-(set-version (list 0.0 1)
-             (list 0.1 0))
-(set-revision-date 2007 8 5)
-
 ;;;;;;;;;;;;;;;;
 ; environments ;
 ;;;;;;;;;;;;;;;;
@@ -19,6 +15,11 @@
 ;   'alist is a list of keyed-bindings, constructed by tools in file
 ; "subfiles/keyed.scm".
 ;
+
+(library (subfiles environment)
+  (export)
+  (import (rnrs)
+          (rnrs mutable-pairs))
 
 ;
 ; private constructor/accessors
@@ -41,54 +42,60 @@
 ; public constructor/accessors
 ;
 
-(define make-environment
-  (lambda parents
-    (internal-make-environment
-      (cons (make-empty-frame)
-            (apply append
-                   (map get-environment-frames
-                        parents)))
-      (apply merge-alists
-             (map get-environment-alist
-                  parents)))))
+; XXX: MERGE-ALISTS
+;; (define make-environment
+;;   (lambda parents
+;;     (internal-make-environment
+;;       (cons (make-empty-frame)
+;;             (apply append
+;;                    (map get-environment-frames
+;;                         parents)))
+;;       (apply merge-alists
+;;              (map get-environment-alist
+;;                   parents)))))
 
-(define make-standard-environment
-  (lambda ()
-    (make-environment ground-environment)))
+; XXX: depends on above
+;; (define make-standard-environment
+;;   (lambda ()
+;;     (make-environment ground-environment)))
 
-(define make-environment-with-keyed-binding
-  (lambda (key value parent)
-    (internal-make-environment
-      (cons (make-empty-frame) (get-environment-frames parent))
-      (make-alist (get-environment-alist parent) key value))))
+; XXX: MAKE-ALIST
+;; (define make-environment-with-keyed-binding
+;;   (lambda (key value parent)
+;;     (internal-make-environment
+;;       (cons (make-empty-frame) (get-environment-frames parent))
+;;       (make-alist (get-environment-alist parent) key value))))
 
-(define environment? (make-object-type-predicate 'environment))
+; XXX: object
+;; (define environment? (make-object-type-predicate 'environment))
 
-(define environment-keyed-lookup
-  (lambda (key env context)
-    (let ((binding  (alist-lookup key (get-environment-alist env))))
-      (if (pair? binding)
-          (cdr binding)
-          (error-pass
-            (make-error-descriptor
-              "Attempted to look up an unbound keyed static variable"
-              (list "in " (list env)))
-            context)))))
+; XXX: ALIST-LOOKUP
+;; (define environment-keyed-lookup
+;;   (lambda (key env context)
+;;     (let ((binding  (alist-lookup key (get-environment-alist env))))
+;;       (if (pair? binding)
+;;           (cdr binding)
+;;           (error-pass
+;;             (make-error-descriptor
+;;               "Attempted to look up an unbound keyed static variable"
+;;               (list "in " (list env)))
+;;             context)))))
 
 ;
 ; Returns the value bound to name if there is one, otherwise throws an error.
 ;
-(define lookup
-  (lambda (name env context)
-    (let ((binding  (get-binding-from-frames
-                      name (get-environment-frames env))))
-      (if (eq? binding #f)
-          (error-pass
-            (make-error-descriptor
-              (list "Unbound symbol: " (symbol->string name))
-              (list "in " (describe-object env)))
-            context)
-          (cdr binding)))))
+; XXX: ERROR-PASS
+;; (define lookup
+;;   (lambda (name env context)
+;;     (let ((binding  (get-binding-from-frames
+;;                       name (get-environment-frames env))))
+;;       (if (eq? binding #f)
+;;           (error-pass
+;;             (make-error-descriptor
+;;               (list "Unbound symbol: " (symbol->string name))
+;;               (list "in " (describe-object env)))
+;;             context)
+;;           (cdr binding)))))
 
 ;
 ; Performs a series of local bind operations, mutating existing local bindings
@@ -98,11 +105,12 @@
 ; Each later odd argument is the value to be locally bound to the immediately
 ; preceding name.
 ;
-(define add-bindings!
-  (lambda (env . x)
-    (apply add-bindings-to-frame!
-           (car (get-environment-frames env))
-           x)))
+; XXX: ADD-BINDINGS-TO-FRAME!  (SF)
+;; (define add-bindings!
+;;   (lambda (env . x)
+;;     (apply add-bindings-to-frame!
+;;            (car (get-environment-frames env))
+;;            x)))
 
 ;
 ; Removes local bindings for given symbols.  The first argument is the
@@ -127,25 +135,26 @@
 ; A parameter tree is valid if it is acyclic, it contains no duplicate symbols,
 ; and every leaf is either a symbol, nil, or ignore.
 ;
-(define valid-ptree?
-  (lambda (tree)
+; XXX: CYCLIC-TREE?
+;; (define valid-ptree?
+;;   (lambda (tree)
 
-    (define aux ; returns symbols if valid, #f if invalid
-      (lambda (tree symbols)
-        (cond ((ignore? tree)  symbols)
-              ((null? tree)    symbols)
-              ((symbol? tree)  (if (pair? (member tree symbols))
-                                   #f
-                                   (cons tree symbols)))
-              ((kernel-pair? tree)
-                 (let ((symbols  (aux (kernel-car tree) symbols)))
-                   (if (eq? symbols #f)
-                       #f
-                       (aux (kernel-cdr tree) symbols))))
-              (else  #f))))
+;;     (define aux ; returns symbols if valid, #f if invalid
+;;       (lambda (tree symbols)
+;;         (cond ((ignore? tree)  symbols)
+;;               ((null? tree)    symbols)
+;;               ((symbol? tree)  (if (pair? (member tree symbols))
+;;                                    #f
+;;                                    (cons tree symbols)))
+;;               ((kernel-pair? tree)
+;;                  (let ((symbols  (aux (kernel-car tree) symbols)))
+;;                    (if (eq? symbols #f)
+;;                        #f
+;;                        (aux (kernel-cdr tree) symbols))))
+;;               (else  #f))))
 
-    (and (not (cyclic-tree? tree))
-         (list? (aux tree ())))))
+;;     (and (not (cyclic-tree? tree))
+;;          (list? (aux tree ())))))
 
 ;
 ; Locally binds a parameter-tree to an object.
@@ -154,71 +163,73 @@
 ; parameter-tree/object mismatch), returns an error-descriptor to whose
 ; first line " when calling ..." might reasonably be appended.
 ;
-(define match!
-  (lambda (env ptree object)
+; XXX: VALID-PTREE?
+;; (define match!
+;;   (lambda (env ptree object)
 
-    (define emsg ()) ; repository for error-descriptor content
+;;     (define emsg ()) ; repository for error-descriptor content
 
-    ; returns arguments for add-bindings-to-frame!
-    (define aux
-      (lambda (ptree object args)
-        (cond ((kernel-pair? ptree)
-                 (if (kernel-pair? object)
-                     (aux      (kernel-cdr ptree) (kernel-cdr object)
-                          (aux (kernel-car ptree) (kernel-car object) args))
-                     (set! emsg
-                           (append emsg
-                                   (list (list "  mismatch:  " (list ptree)
-                                               "  " (list object)))))))
-              ((symbol? ptree)  (cons ptree (cons object args)))
-              ((null? ptree)   (if (null? object)
-                                   args
-                                   (set! emsg
-                                         (append emsg
-                                            (list (list "  mismatch:  ()  "
-                                                        (list object)))))))
-              (else args)))) ; must be ignore
+;;     ; returns arguments for add-bindings-to-frame!
+;;     (define aux
+;;       (lambda (ptree object args)
+;;         (cond ((kernel-pair? ptree)
+;;                  (if (kernel-pair? object)
+;;                      (aux      (kernel-cdr ptree) (kernel-cdr object)
+;;                           (aux (kernel-car ptree) (kernel-car object) args))
+;;                      (set! emsg
+;;                            (append emsg
+;;                                    (list (list "  mismatch:  " (list ptree)
+;;                                                "  " (list object)))))))
+;;               ((symbol? ptree)  (cons ptree (cons object args)))
+;;               ((null? ptree)   (if (null? object)
+;;                                    args
+;;                                    (set! emsg
+;;                                          (append emsg
+;;                                             (list (list "  mismatch:  ()  "
+;;                                                         (list object)))))))
+;;               (else args)))) ; must be ignore
 
-    (if (not (valid-ptree? ptree))
-        (make-error-descriptor "Invalid parameter tree"
-                               (list "Parameter tree: " (list ptree)))
-        (let ((args  (aux ptree object ())))
-          (if (pair? emsg)
-              (apply make-error-descriptor
-                     "Definiend/object mismatch"
-                     (list "Definiend:  " (list ptree))
-                     (list "Object:     " (list object))
-                     (list)
-                     emsg)
-              (begin
-                (apply add-bindings-to-frame!
-                       (car (get-environment-frames env))
-                       args)
-                ()))))))
+;;     (if (not (valid-ptree? ptree))
+;;         (make-error-descriptor "Invalid parameter tree"
+;;                                (list "Parameter tree: " (list ptree)))
+;;         (let ((args  (aux ptree object ())))
+;;           (if (pair? emsg)
+;;               (apply make-error-descriptor
+;;                      "Definiend/object mismatch"
+;;                      (list "Definiend:  " (list ptree))
+;;                      (list "Object:     " (list object))
+;;                      (list)
+;;                      emsg)
+;;               (begin
+;;                 (apply add-bindings-to-frame!
+;;                        (car (get-environment-frames env))
+;;                        args)
+;;                 ()))))))
 
 ;
 ; Constructs an empty frame.
 ;
-(define make-empty-frame (lambda () (list ())))
+(define make-empty-frame (lambda () (list '())))
 
 ;
 ; Performs a series of bind operations in given frame, mutating existing
 ; bindings when possible, creating new bindings otherwise.  Arguments as
 ; add-bindings!, except that the first argument is the local frame.
 ;
-(define add-bindings-to-frame!
-  (lambda (frame . x)
-    (if (pair? x)
-        (let ((name   (car  x))
-              (value  (cadr x))
-              (x      (cddr x)))
-          (if (object? value)
-              (suggest-object-name value name))
-          (let ((binding  (get-binding-from-frame name frame)))
-            (if (eq? binding #f)
-                (set-car! frame (cons (cons name value) (car frame)))
-                (set-cdr! binding value)))
-          (apply add-bindings-to-frame! frame x)))))
+; XXX: OBJECT?
+;; (define add-bindings-to-frame!
+;;   (lambda (frame . x)
+;;     (if (pair? x)
+;;         (let ((name   (car  x))
+;;               (value  (cadr x))
+;;               (x      (cddr x)))
+;;           (if (object? value)
+;;               (suggest-object-name value name))
+;;           (let ((binding  (get-binding-from-frame name frame)))
+;;             (if (eq? binding #f)
+;;                 (set-car! frame (cons (cons name value) (car frame)))
+;;                 (set-cdr! binding value)))
+;;           (apply add-bindings-to-frame! frame x)))))
 
 ;
 ; Deletes bindings for given symbols from given frame.  Arguments as
@@ -265,3 +276,9 @@
 (define get-binding-from-frame
   (lambda (name frame)
     (assoc name (car frame))))
+
+;; (set-version (list 0.0 1)
+;;              (list 0.1 0))
+;; (set-revision-date 2007 8 5)
+
+)
