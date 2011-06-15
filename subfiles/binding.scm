703c8c8640f14a61e60b948ff9c55ac9e1400cd8
David Banks <amoebae@gmail.com>
2011-06-15 16:26:56 +0100

    valid: inert; fix ignore bindings

--- subfiles/binding.scm
+++ subfiles/binding.scm
@@ -1061,4 +1061,20 @@
 
       )))
 
+
+;
+; Creates bindings for this type in a given environment.
+;
+; This code should not use any internal knowledge of the ignore type (not
+; that there's anything much to use).  It appears in this file, rather than
+; in "subfiles/ground.scm", simply because it is logically associated with
+; the ignore type.
+;
+(define bind-ignore-primitives!
+  (lambda (env)
+    (add-bindings! env
+
+      'ignore?  (unary-predicate->applicative ignore?))))
+
+
 )
