7fa59013e95677835d42e6588150227453545b87
David Banks <amoebae@gmail.com>
2011-06-15 16:24:26 +0100

    valid: ignore

--- subfiles/ignore.scm
+++ subfiles/ignore.scm
@@ -1,10 +1,6 @@
 ; This file is part of SINK, a Scheme-based Interpreter for Not-quite Kernel
 ; Copyright (c) 2009 John N. Shutt
 
-(set-version (list 0.0 0)
-             (list 0.1 0))
-(set-revision-date 2007 8 4)
-
 ;;;;;;;;;;
 ; ignore ;
 ;;;;;;;;;;
@@ -12,10 +8,24 @@
 ; The ignore value has type 'ignore and no attributes.
 ;
 
+(library (subfiles ignore)
+  (export)
+  (import (rnrs))
+
 (define ignore (let ((name  (list #f)))
                  (lambda (message)
                    (case message
                      ((type) 'ignore)
                      ((name) name)))))
 
-(define ignore? (make-object-type-predicate 'ignore))
+; XXX
+;(define ignore? (make-object-type-predicate 'ignore))
+
+)
+
+
+; XXX
+;; (set-version (list 0.0 0)
+;;              (list 0.1 0))
+;; (set-revision-date 2007 8 4)
+
