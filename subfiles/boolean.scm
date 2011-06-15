12b51db9e797ce9a35d7061c6c45d254c822d73e
David Banks <amoebae@gmail.com>
2011-06-15 16:22:36 +0100

    valid: boolean

--- subfiles/boolean.scm
+++ subfiles/boolean.scm
@@ -1,13 +1,14 @@
 ; This file is part of SINK, a Scheme-based Interpreter for Not-quite Kernel
 ; Copyright (c) 2009 John N. Shutt
 
-(set-version (list 0.0 0)
-             (list 0.1 0))
-(set-revision-date 2007 8 5)
 
 ;;;;;;;;;;;;
 ; booleans ;
 ;;;;;;;;;;;;
+
+(library (subfiles boolean)
+  (export)
+  (import (rnrs))
 
 ;
 ; Given zero or more boolean arguments, returns their conjunction (i.e.,
@@ -18,3 +19,10 @@
     (or (null? ls)
         (and (car ls)
              (apply and? (cdr ls))))))
+
+; XXX
+;(set-version (list 0.0 0)
+;             (list 0.1 0))
+;(set-revision-date 2007 8 5)
+
+)
